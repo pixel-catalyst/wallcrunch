1335a83ef4740c3ba6d2c021aec2bc43639fbc88
Ashwin <ashwin.aksharma.p@outlook.com>
2025-06-19 21:24:19 +0530

    cleaning

--- out/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ExCeXGeZl4hmpesUH14jAREBeyxzg6vh2Dh8MdozTWo=\"\n}"
--- out/static/chunks/src_app_page_tsx_8450df25._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_fcd9a6a1._.js",
-  "static/chunks/src_db374125._.js"
-],
-    source: "dynamic"
-});
